942fbb83da9ea6a7256ede2fa6b353239accc1f4
PaulEH <paulmuenzner@web.de>
2024-01-29 17:20:53 +0100

    Add unit test for DeleteLocalFolder()

---
+++ utils/files/delete_files_test.go
@@ -0,0 +1,40 @@
+package files
+
+import (
+	"os"
+	"testing"
+)
+
+func TestDeleteLocalFolder(t *testing.T) {
+	// Create a temporary folder for testing
+	testFolder := "test_folder"
+	err := os.Mkdir(testFolder, 0777)
+	if err != nil {
+		t.Fatalf("Failed to create test folder: %v", err)
+	}
+	defer os.RemoveAll(testFolder)
+
+	tests := []struct {
+		name     string
+		folder   string
+		expected error
+	}{
+		{"DeleteExistingFolder", testFolder, nil},
+		{"DeleteNonExistentFolder", "nonexistent_folder", nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := DeleteLocalFolder(tt.folder)
+
+			if (err != nil) != (tt.expected != nil) {
+				t.Errorf("DeleteLocalFolder() error = %v, expected %v", err, tt.expected)
+				return
+			}
+
+			if err != nil && err.Error() != tt.expected.Error() {
+				t.Errorf("DeleteLocalFolder() error = %v, expected %v", err, tt.expected)
+			}
+		})
+	}
+}
