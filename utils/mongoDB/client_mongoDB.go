ab2d989c2587a9c6032c1e68be8b8a1091221773
PaulEH <paulmuenzner@web.de>
2024-02-01 16:22:52 +0100

    Cleanup comments database client function

--- utils/mongoDB/client_mongoDB.go
+++ utils/mongoDB/client_mongoDB.go
@@ -81,35 +81,3 @@ func (mongoDBClientConfig *MongoDBClientConfigData) GetURI() (string, error) {
 
 	return uri, nil
 }
-
-// func ConnectToMongoDB(mongodbURI string) (*MongoDBClient, error) {
-
-// 	commandStarted := []string{}
-// 	cmdMonitor := &event.CommandMonitor{
-// 		Started: func(_ context.Context, evt *event.CommandStartedEvent) {
-// 			commandStarted = append(commandStarted, evt.CommandName)
-// 		},
-// 	}
-
-// 	clientOptions := options.Client().ApplyURI(mongodbURI).
-// 		// Add your security settings here (e.g., clientOptions.SetAuth)
-// 		SetMaxPoolSize(50).
-// 		SetReadConcern(readconcern.Local()).
-// 		SetWriteConcern(writeconcern.Majority()).
-// 		SetRetryWrites(true).
-// 		SetCompressors([]string{"zstd", "snappy"}).
-// 		SetMonitor(cmdMonitor)
-
-// 	client, err := mongo.Connect(context.Background(), clientOptions)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("Client setup MongoDB failed in 'ConnectToMongoDB()' utilizing 'mongo.Connect()'. Error: %v", err)
-// 	}
-
-// 	// Check the connection
-// 	err = client.Ping(context.TODO(), nil)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("Error in 'ConnectToMongoDB()' utilizing 'client.Ping()'. Cannot connect to MongoDB. Error: %v", err)
-// 	}
-
-// 	return &MongoDBClient{MongoDB: client}, nil
-// }
